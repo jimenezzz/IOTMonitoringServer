bc2a8955db2a79d7b0cabff7e2b5b05eff55e4cd
jimenezzz <j.jimenez21@uniandes.edu.co>
2023-09-16 19:15:41 -0500

    se elimina analy_data

--- control/monitor.py
+++ control/monitor.py
@@ -159,7 +159,7 @@ def start_cron():
     Inicia el cron que se encarga de ejecutar la función analyze_data cada 5 minutos.
     '''
     print("Iniciando cron...")
-    schedule.every(5).minutes.do(analyze_data)
+    #schedule.every(5).minutes.do(analyze_data)
     schedule.every(2).minutes.do(control_leds)
     print("Servicio de control iniciado")
     while 1:
